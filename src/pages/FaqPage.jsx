74915d01e12231b1a16daa5ac62b1c811572b541
Arenga Pinnata Marzuki <arengapinnatamarzuki@gmail.com>
2023-10-07 13:55:40 +0700

    Revisi lagi

--- src/pages/FaqPage.jsx
+++ src/pages/FaqPage.jsx
@@ -2,7 +2,7 @@ import FaqComponent from "../components/FaqComponent";
 
 export const FaqPage = () => {
   return (
-    <div className="pt-5 animate__animated animate__fadeInUp">
+    <div className="animate__animated animate__fadeInUp">
       <FaqComponent />
     </div>
   );
